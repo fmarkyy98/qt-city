6f3b057bd1aa0ec74ec2bfc9e5bbc0d905f485a8
se3mann <janosmark11@gmail.com>
2023-04-03 13:10:43 +0200

    some fix

--- cmake/platforms.cmake
+++ cmake/platforms.cmake
@@ -4,14 +4,13 @@ if(UNIX AND NOT APPLE)
 endif()
 
 if(WIN32) 
-    set(CMAKE_CXX_COMPILER ${PROJECT_SOURCE_DIR}/external/windows/mingw/bin/g++.exe)
     set(CMAKE_PREFIX_PATH ${PROJECT_SOURCE_DIR}/external/windows/qt/6.4.3/mingw_64)
-    set(Qt6_DIR ${PROJECT_SOURCE_DIR}/external/windows/qt/6.4.3/mingw_64/lib/cmake/Qt6)
+    set(Qt6_DIR ${PROJECT_SOURCE_DIR}/external/windows/qt/6.4.3/mingw_64/)
     set(QT_WINDEPLOY ${PROJECT_SOURCE_DIR}/external/windows/qt/6.4.3/mingw_64/bin/windeployqt.exe)
 elseif(LINUX)
     message(STATUS ">>>Linux")
 elseif(APPLE)
-    #
+    message(STATUS ">>>Apple")
 else()
     # 
 endif()
