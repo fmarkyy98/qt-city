cd4d8e13baa513f37f67f5a3e531bbf95a37e445
se3mann <janosmark11@gmail.com>
2023-04-03 13:26:04 +0200

    added include folders

--- cmake/platforms.cmake
+++ cmake/platforms.cmake
@@ -4,6 +4,8 @@ if(UNIX AND NOT APPLE)
 endif()
 
 if(WIN32) 
+    # To build project without Qt Creator with CMake
+    # Make sure that you have the qt dependencie in <project's root'>/external/windows/qt path
     set(CMAKE_PREFIX_PATH ${PROJECT_SOURCE_DIR}/external/windows/qt/6.4.3/mingw_64)
     set(Qt6_DIR ${PROJECT_SOURCE_DIR}/external/windows/qt/6.4.3/mingw_64/)
     set(QT_WINDEPLOY ${PROJECT_SOURCE_DIR}/external/windows/qt/6.4.3/mingw_64/bin/windeployqt.exe)
