b42eeb28955d90f2e14cafa586670ec1c7b68a58
Solyom <u4xu7i@inf.elte.hu>
2023-04-05 21:54:33 +0200

    new things

--- src/model/IGameModel.h
+++ src/model/IGameModel.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../common/IService.h" // TODO normális útvonalat megadni.
+#include "zone/ZoneType.h"
 
 class IGameModelMeta : public details::IServiceMeta {
 signals:
@@ -12,7 +13,8 @@ signals:
 class IGameModel : public IService<IGameModelMeta>
 {
 public:
-    virtual zoneAt(std::pair<int,int> position) const = 0;
+    virtual ZoneType zoneAt(int row, int col) const = 0;
+    virtual void placeZone(ZoneType zoneType, int row, int col) = 0;
     virtual void save(const QString& path) const = 0;
     virtual void load(const QString& path) = 0;
 };
