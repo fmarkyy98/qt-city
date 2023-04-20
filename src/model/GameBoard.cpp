732ff67a4d7fbfeebfa15dc7273d4ba6372cb6bf
fmarkyy98 <fmarkyy98@gmail.com>
2023-04-20 13:04:35 +0200

    Zone changed to have in structures too, faged enum for ZoneType, PlaceBuilding impl changes.

--- src/model/GameBoard.cpp
+++ src/model/GameBoard.cpp
@@ -23,30 +23,33 @@ Tile &GameBoard::at(std::pair<int, int> position)
 
 void GameBoard::placeBuilding(qct::BuildingType buildingType, std::pair<int, int> position)
 {
-    std::unique_ptr<StructureBase> newBuilding;
+    StructureBase* newBuilding;
     auto [row, col] = position;
     switch (buildingType) {
-    case qct::BuildingType::Residential:
-        newBuilding = std::make_unique<ResidentialBuilding>();
-        break;
-    case qct::BuildingType::Factory:
-        newBuilding = std::make_unique<Factory>();
-        break;
-    case qct::BuildingType::Police:
-        newBuilding = std::make_unique<Police>();
-        break;
-    case qct::BuildingType::Forest:
-        newBuilding = std::make_unique<Forest>();
-        break;
-    case qct::BuildingType::Road:
-        newBuilding = std::make_unique<Road>();
-        break;
-    case qct::BuildingType::Stadium:
-        newBuilding = std::make_unique<Stadium>();
-        break;
-    case qct::BuildingType::Store:
-        newBuilding = std::make_unique<Store>();
-        break;
+    case qct::BuildingType::Residential: {
+        newBuilding = new ResidentialBuilding();
+    } break;
+    case qct::BuildingType::Factory: {
+        newBuilding = new Factory();
+    } break;
+    case qct::BuildingType::Police: {
+        newBuilding = new Police();
+    } break;
+    case qct::BuildingType::Forest: {
+        newBuilding = new Forest();
+    } break;
+    case qct::BuildingType::Road: {
+        newBuilding = new Road();
+    } break;
+    case qct::BuildingType::Stadium: {
+        newBuilding = new Stadium();
+    } break;
+    case qct::BuildingType::Store: {
+        newBuilding = new Store();
+    } break;
+    default: {
+        throw std::invalid_argument("Invalid BuildingType");
+    } break;
     }
     //TODO epulet teljes alapteruletere meg kell nezni hogy kompatibilis zona van e ott,
     //ha ez mindegyikre teljesul, akkor az osszes tile-ra fel kell helyezni a raw pointert(newBuilding.get())
@@ -55,12 +58,22 @@ void GameBoard::placeBuilding(qct::BuildingType buildingType, std::pair<int, int
     if(!newBuilding->canBuildOnZone(m_TileMatrix[row][col].zoneType))
         return;
 
-    m_TileMatrix[row][col].structure = newBuilding.get();
+    m_TileMatrix[row][col].structure = newBuilding;
 
-    if(std::dynamic_pointer_cast<BuildingBase>(newBuilding) != nullptr)
-        m_Buildings.push_back(std::move(newBuilding));
-    else
-        m_Structures.push_back(std::move(newBuilding));
+    switch(newBuilding->getType()) {
+    case qct::BuildingType::Road:
+    case qct::BuildingType::Forest: {
+        m_Structures.push_back(std::unique_ptr<StructureBase>(newBuilding));
+    } break;
+    case qct::BuildingType::Residential:
+    case qct::BuildingType::Factory:
+    case qct::BuildingType::Store:
+    case qct::BuildingType::Police:
+    case qct::BuildingType::Stadium: {
+        m_Buildings.push_back(std::unique_ptr<BuildingBase>(static_cast<BuildingBase*>(newBuilding)));
+    } break;
+    default: {} break;
+    }
 }
 
 void GameBoard::placeZone(qct::ZoneType zoneType, std::pair<int, int> position)
--- src/model/export/Enums.h
+++ src/model/export/Enums.h
@@ -20,12 +20,11 @@ namespace qct {
     Q_ENUM_NS(qct::BuildingType)
 
     enum class ZoneType {
-		Residential,
-		Industrial,
-		Service,
-		None,
-
-		Count // Cool trick to get the count of the enum values.
-	};        // They must not be specified explicitly.
+        None = 1,
+        Residential = 2,
+        Industrial = 4,
+        Service = 8,
+        All = 15
+    };
     Q_ENUM_NS(qct::ZoneType)
 }
